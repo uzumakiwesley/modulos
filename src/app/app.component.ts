d6e48c0d77c0935e3aaf8ae1251c25edec8b2969
uzumakiwesley <uzumakiwesley58@gmail.com>
2022-10-25 21:48:08 -0300

    commit modulos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'formularios';
+  dadosUsuario = {
+    nome: '',
+    sobrenome: '',
+    email: '',
+    senha: ''
+  }
 }
